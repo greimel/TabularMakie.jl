b82866374aa3b37e0e8574ed28f9efa9cfdddedc
Documenter.jl <documenter@juliadocs.github.io>
2021-01-19 12:41:45 +0000

    build based on 3e05995

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TabularMakie","category":"page"},{"location":"#TabularMakie","page":"Home","title":"TabularMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TabularMakie]","category":"page"}]
+}
