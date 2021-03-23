5f7d6f4d43e44f7aa6c5c60029da2af058a66385
Fabian Greimel <fabgrei@gmail.com>
2021-03-23 11:46:14 +0100

    use categorical_fun from AbstractPlotting

--- src/TabularMakie.jl
+++ src/TabularMakie.jl
@@ -7,13 +7,12 @@ using AbstractPlotting: MakieLayout
 using AbstractPlotting.MakieLayout: Optional, LegendEntry, EntryGroup
 # import some internal functions of AbstractPlotting
 # TODO: test behavior of these functions
-using AbstractPlotting: categorical_labels, categorical_range
+using AbstractPlotting: categorical_labels, categorical_range, categorical_positions, categorical_trait, Continuous, Automatic
 
 using DataFrames
 using Underscores: @_
 using StructArrays
 using UnPack: @unpack
-using DataAPI: refarray
 #using Statistics
 using NamedTupleTools: delete
 
