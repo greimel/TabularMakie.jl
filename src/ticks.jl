745c2710ff3b7c1e8054834faa32499f48ec289e
Fabian Greimel <fabgrei@gmail.com>
2021-04-05 11:07:52 +0200

    remove comment

--- src/ticks.jl
+++ src/ticks.jl
@@ -12,8 +12,8 @@ function categorical_ticks!(ax, x, y)
 end
 
 function categorical_ticks(var)
-	ticks  = categorical_range(var)  # internal function from AbstractPlotting
-	labels = categorical_labels(var) # internal function from AbstractPlotting
+	ticks  = categorical_range(var)
+	labels = categorical_labels(var)
 
 	if ticks isa Automatic
 		nothing
