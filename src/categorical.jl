52a9f3779fa9052ddd36f9f26b75e8c7d212d6cf
Fabian Greimel <fabgrei@gmail.com>
2021-04-05 11:15:09 +0200

    move functionality from JuliaPlots/AbstractPlotting#667

--- src/categorical.jl
+++ src/categorical.jl
@@ -1,51 +1,40 @@
-module TmpCategorical
+module CategoricalConversions
 
 export Continuous, HasRefPool, Categorical,
     catetorical_labels, categorical_range, categorical_trait, categorical_positions
 
 using DataAPI: DataAPI
-using AbstractPlotting: AbstractPlotting, categorical_trait, Categorical, Automatic
+using AbstractPlotting: Automatic
 
-const AP = AbstractPlotting
+# Trait for categorical values
+struct Categorical end
+struct Continuous end
+struct HasRefPool end # the better Categorical
 
-struct HasRefPool end
-const Continuous = AP.Continous
+categorical_trait(x::AbstractVector) = !isnothing(DataAPI.refpool(x)) ? HasRefPool() : Categorical()
+categorical_trait(::AbstractVector{<: Number}) = Continuous()
 
-AP.categorical_trait(x::AbstractVector) = !isnothing(DataAPI.refpool(x)) ? HasRefPool() : Categorical()
-AP.categorical_trait(x::AbstractVector{<: Number}) = Continuous()
-
-AP.categoric_labels(::HasRefPool,  xs) = DataAPI.levels(xs)
-
-categorical_labels = AP.categoric_labels
-
-# -----------------------------
-# ----- Categorical range -----
-# -----------------------------
+categorical_labels(xs) = categorical_labels(categorical_trait(xs), xs)
+categorical_labels(::Categorical, xs) = unique(xs)
+categorical_labels(::Continuous,  _)  = automatic # we let them be automatic
+categorical_labels(::HasRefPool,  xs) = DataAPI.levels(xs) # could also use values(DataAPI.refpool(xs))
 
 categorical_range(xs) = categorical_range(categorical_trait(xs), xs)
-
-function categorical_range(t, xs)
-    labels  = categorical_labels(t, xs)
-    AP.categoric_range(labels)
-end
-
-#categorical_range(::HasRefPool,  xs) = keys(DataAPI.refpool(xs))
-
-# -----------------------------
-# --- Categorical positions ---
-# -----------------------------
-
-categorical_positions(xs) = categorical_positions(categorical_trait(xs), xs)
-categorical_positions(::Continuous, xs) = xs
-function categorical_positions(t::AP.Categorical, xs)
-    labels = categorical_labels(xs)
-    categorical_position.(Ref(t), xs, Ref(xs), Ref(labels))
-end
-categorical_positions(::HasRefPool, xs) = DataAPI.refarray(xs)
+categorical_range(::Categorical, xs) = 1:length(categorical_labels(xs))
+categorical_range(::Continuous,  _)  = automatic # we let them be automatic
+categorical_range(::HasRefPool,  xs) = keys(DataAPI.refpool(xs))
 
 categorical_position(x, xs) = categorical_position(categorical_trait(xs), x, xs)
 categorical_position(::Categorical, x, xs, labels = categorical_labels(xs)) = findfirst(l -> l == x, labels)
 categorical_position(::Continuous,  x, _)  = x
 categorical_position(::HasRefPool,  x, xs) = DataAPI.invrefpool(xs)[x]
+
+categorical_positions(xs) = categorical_positions(categorical_trait(xs), xs)
+categorical_positions(::Continuous, xs) = xs
+function categorical_positions(t::Categorical, xs)
+    labels = categorical_labels(xs)
+    categorical_position.(Ref(t), xs, Ref(xs), Ref(labels))
+end
+categorical_positions(::HasRefPool, xs) = DataAPI.refarray(xs)
 
 end
